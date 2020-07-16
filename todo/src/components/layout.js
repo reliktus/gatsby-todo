deb1fee5a4fdba6f1aa93dc8f949fe6519a73dfe
relikt <reliktus@gmail.com>
2020-07-16 17:17:55 +0200

    layout refactor

--- todo/src/components/layout.js
+++ todo/src/components/layout.js
@@ -9,17 +9,18 @@ export default function Layout({ crumbs, customCrumbLabel, children }) {
 	const data = query();
 
 	return (
-		<div className="bg-gray-300 text-2xl text-center content-center">
-			<header className="mb-2">
+		<div className="bg-gray-300 text-2xl text-center">
+			<header className="mb-2 flex flex-row justify-center">
 				<Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
 					<h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
 				</Link>
-				<Breadcrumb crumbs={crumbs} crumbSeparator=" > " crumbLabel={customCrumbLabel} />
+				[<Breadcrumb crumbs={crumbs} crumbSeparator=" > " crumbLabel={customCrumbLabel} />]
 				<Menu />
 			</header>
-			{children}
+			<section className="">{children}</section>
 			<footer className="he">
-				<Menu />
+				{/* <Menu /> */}
+				Impressum | Copyright 2020
 			</footer>
 		</div>
 	);
--- todo/src/pages/index.js
+++ todo/src/pages/index.js
@@ -7,7 +7,13 @@ export default ({ pageContext, location }) => {
 
 	return (
 		<Layout crumbs={crumbs} crumbLabel={customCrumbLabel}>
-			<img src="https://source.unsplash.com/random/400x200" alt="" />
+			<container className="flex flex-col">
+				<section className="flex justify-center">
+					<img src="https://source.unsplash.com/random/400x200" alt="" />
+				</section>
+				<section>Projects</section>
+				<section>technologies</section>
+			</container>
 		</Layout>
 	);
 };
