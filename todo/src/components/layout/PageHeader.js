b60d48c475209cc921f0ada89c81fb7639454b2a
Rafał Jaworski <rjaworski@sii.pl>
2020-11-17 11:28:45 +0100

    local storage handler coloro manager

--- todo/src/components/layout/PageHeader.js
+++ todo/src/components/layout/PageHeader.js
@@ -1,31 +1,30 @@
 import { Breadcrumb } from 'gatsby-plugin-breadcrumb';
 import { Link } from 'gatsby';
 import React from 'react';
-import query from '../query/layoutQuery';
 import Menu from '../menu';
+import logo from '../../../static/images/rj.png';
 
-export default function PageHeader({crumbs, customCrumbLabel}) {
-    const data = query();
-    const outsideHome = () => crumbs.length > 1;
-    const breadcrumbEl = () => (
-		<div className="flex text-base flex-row flex-no-wrap">
+export default function PageHeader({ crumbs, customCrumbLabel }) {
+	const outsideHome = () => crumbs.length > 1;
+	const breadcrumbEl = () => (
+		<div className="flex text-base flex-row flex-no-wrap divide-y-2 divide-gray-300">
 			Location:<Breadcrumb
 				crumbs={crumbs}
-				crumbSeparator="/"
+				crumbSeparator="."
 				crumbLabel={customCrumbLabel}
 				className="flex flex-row flex-no-wrap text-base "
 			/>
 		</div>
 	);
-    return (
-        <header className="bg-gray-300 p-4 flex flex-col items-stretch">
-				<div className="flex justify-between sm:min-w-full lg:max-w-screen-md">
-					<Link to="/" className="pr-4 hover:text-blue-800">
-						<h3>[{data.site.siteMetadata.title}]</h3>
-					</Link>
-					<Menu className="hover:text-blue-800" />
-				</div>
-				{outsideHome() && breadcrumbEl()}
-			</header>
-    )
+	return (
+		<header className="bg-gray-300 p-4 flex flex-col items-stretch">
+			<div className="flex justify-between sm:min-w-full lg:max-w-screen-md">
+				<Link to="/" className="h-10 w-10">
+					<img className="-ml-2 inline-block h-10 w-10 rounded-full text-white shadow-solid" src={logo} alt="" />
+				</Link>
+				<Menu className="hover:text-blue-800" />
+			</div>
+			{outsideHome() && breadcrumbEl()}
+		</header>
+	)
 }
--- todo/src/components/panel/ColorModeSwitch.js
+++ todo/src/components/panel/ColorModeSwitch.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import LocalStorageHandler from '../services/LocalStorageHandler';
+import React, { useState } from 'react';
+import LocalStorageHandler from '../../utils/LocalStorageHandler';
 
 export default function ColorModeSwitch() {
-    const PAGE_COLOR_THEME_KEY = 'pageColorMode'; // dark or default
+    const body = document.body;
+    const DARK_MODE_ACTIVE = 'pageColorMode';
     const LocalStorage = new LocalStorageHandler();
-    let checkBoxState = true;
-    const colorChange = (e) => {
-        let body = document.body;
-        // mode === 'dark' ? body.classList.add('dark') : body.classList.remove('dark');
-
-        // console.log(e.currentTarget.checked);
-        checkBoxState ? body.classList.add('dark') : body.classList.remove('dark');
-        LocalStorage.setLocalStorageKeyValue(PAGE_COLOR_THEME_KEY, checkBoxState ? 'dark' : 'default');
+    const darkModeStateInLS = LocalStorage.getLocalStorageKeyValue(DARK_MODE_ACTIVE) === 'true' ? true : false;
+    const [darkMode, setDarkModeState] = useState(darkModeStateInLS);
+    darkMode ? body.classList.add('dark') : body.classList.remove('dark');
+    const setDarkMode = () => {
+        LocalStorage.setLocalStorageKeyValue(DARK_MODE_ACTIVE, !darkMode);
+        setDarkModeState(!darkMode);
     };
-    LocalStorage.getLocalStorageKeyValue('pageColorMode') && colorChange(LocalStorage.getLocalStorageKeyValue('pageColorMode'))
     return (
         <div>
             <div className="flex items-center justify-center w-full">
                 <label htmlFor="toogleA" className="flex items-center cursor-pointer" >
                     <div className="relative">
-                        <input id="toogleA" type="checkbox" defaultChecked={checkBoxState} className="hidden" onClick={(e) => colorChange(e)} />
+                        <input id="toogleA" type="checkbox" defaultChecked={darkMode} className="hidden" onClick={() => setDarkMode()} />
                         <div className="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
                         <div className="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
                     </div>
