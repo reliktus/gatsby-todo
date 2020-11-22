2565fd55b6b9b11887a1e22d3be39c09b4a2232b
relikt <reliktus@gmail.com>
2020-11-22 19:26:03 +0100

    minor formating

--- todo/src/components/layout.js
+++ todo/src/components/layout.js
@@ -6,7 +6,7 @@ import PageHeader from './layout/PageHeader';
 export default function Layout({ crumbs, customCrumbLabel, children }) {
 	return (
 		<div className="text-2xl flex flex-col min-h-screen bg-gray-400">
-			<PageHeader crumbs={crumbs} customCrumbLabel={customCrumbLabel}/>
+			<PageHeader crumbs={crumbs} customCrumbLabel={customCrumbLabel} />
 			<section className="flex-1 p-4">{children}</section>
 			<PageFooter />
 		</div>
