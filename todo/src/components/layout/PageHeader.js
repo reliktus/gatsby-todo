f84c683c97e0445988d42a361a3127f2f443ebf7
Rafał J <reliktus@gmail.com>
2021-02-04 22:35:16 +0100

    disable breadcrumbs

--- todo/src/components/layout/PageHeader.js
+++ todo/src/components/layout/PageHeader.js
@@ -1,21 +1,21 @@
-import { Breadcrumb } from 'gatsby-plugin-breadcrumb';
+// import { Breadcrumb } from 'gatsby-plugin-breadcrumb';
 import { Link } from 'gatsby';
 import React from 'react';
 import Menu from '../menu';
 import logo from '../../../static/images/rj.png';
 
 export default function PageHeader({ crumbs, customCrumbLabel }) {
-	const outsideHome = () => crumbs.length > 1;
-	const breadcrumbEl = () => (
-		<div className="flex text-base flex-row flex-no-wrap divide-y-2 divide-gray-300">
-			Location:<Breadcrumb
-				crumbs={crumbs}
-				crumbSeparator="."
-				crumbLabel={customCrumbLabel}
-				className="flex flex-row flex-no-wrap text-base "
-			/>
-		</div>
-	);
+	// const outsideHome = () => crumbs.length > 1;
+	// const breadcrumbEl = () => (
+	// 	<div className="flex text-base flex-row flex-no-wrap divide-y-2 divide-gray-300">
+	// 		Location:<Breadcrumb
+	// 			crumbs={crumbs}
+	// 			crumbSeparator="."
+	// 			crumbLabel={customCrumbLabel}
+	// 			className="flex flex-row flex-no-wrap text-base "
+	// 		/>
+	// 	</div>
+	// );
 	return (
 		<header className="bg-gray-300 p-4 flex flex-col items-stretch">
 			<div className="flex justify-between sm:min-w-full lg:max-w-screen-md">
@@ -24,7 +24,7 @@ export default function PageHeader({ crumbs, customCrumbLabel }) {
 				</Link>
 				<Menu className="hover:text-blue-800" />
 			</div>
-			{outsideHome() && breadcrumbEl()}
+			{/* {outsideHome() && breadcrumbEl()} */}
 		</header>
 	)
 }
