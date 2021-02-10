a539258728ab3596b0e98f17a3eb96da6c1dde62
Rafal J <reliktus@gmail.com>
2021-02-10 16:27:37 +0100

    test change

--- todo/src/components/layout.js
+++ todo/src/components/layout.js
@@ -8,6 +8,7 @@ export default function Layout({ crumbs, customCrumbLabel, children }) {
 		<div className="text-2xl flex flex-col min-h-screen bg-gray-400">
 			<PageHeader crumbs={crumbs} customCrumbLabel={customCrumbLabel} />
 			<section className="flex-1 p-4">{children}</section>
+
 			<PageFooter />
 		</div>
 	);
